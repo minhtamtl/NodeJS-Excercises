7a339c452335f60dc6e25b6550e0eb70d25554f6
Minh <tailoctam2@gmail.com>
2021-09-16 23:14:10 +0700

    Learn about Template engine

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+app.use(morgan('combined'));
+app.engine('.hbs', exphbs({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'resource/views'), 'views');
+
+app.get('/', function(req, res) {
+    res.render('home');
+});
+app.get('/news', function(req, res) {
+    res.render('news');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+})
